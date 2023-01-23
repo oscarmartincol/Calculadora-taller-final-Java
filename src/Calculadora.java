a3ab520d3e019ab48579fcff144892d95f278ae0
Oscar Martinez <oscarmartincol@outlook.com>
2023-01-23 13:00:34 -0500

    Calculadora realiza las operaciones correctamente.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Calculadora {
     // Atributos
     private double num;
-    private double result;
+    private double resultBefore;
+    private  double resultNow;
 // Constructor donde se inicializa el atributo resultado en cero.
     public Calculadora(){
-        this.result = 0;
+        this.resultNow = 0;
     }
 // Método para asignar valor al atributo num
     public void setNum(double num) {
@@ -14,31 +17,52 @@ public class Calculadora {
     // Metodos de la calculadora
 
     private void sumar(){
-        this.result += this.num;
+        this.resultNow += this.num;
     }
 
     private void restar(){
-        this.result -= this.num;
+        this.resultNow -= this.num;
     }
 
     private void multiplicar(){
-        this.result *= this.num;
+        this.resultNow *= this.num;
     }
 
     private void dividir(){
         // Si el número ingresado es diferente de cero se hace la división de lo contrario arroja error.
         if(this.num != 0) {
-            this.result /= this.num;
+            this.resultNow /= this.num;
         }else {
-            System.out.println("Error: no se puede dividor por cero");
+            System.out.println("Error: no se puede dividir por cero");
         }
     }
 
     private void modulo(){
-        this.result %= this.num;
+        this.resultNow %= this.num;
     }
 
-    private void seleccionarOperacion(String operacion){
-
+    public String ejecutarOperacion(String operacion){
+        Scanner input = new Scanner(System.in);
+        double num2 = 0;
+        if(this.resultNow > 0.0){
+            this.resultBefore = this.resultNow;
+            seleccionarOperacion(operacion, input);
+            return this.resultBefore + operacion + this.num + "=" + this.resultNow ;
+        }else {
+            System.out.println("Ingrese el primer número");
+            num2 = input.nextDouble();
+            this.resultNow = num2;
+            seleccionarOperacion(operacion, input);
+            return num2 + operacion + this.num + "=" + this.resultNow;
+        }
     }
+
+    private void seleccionarOperacion(String operacion, Scanner input){
+        if(operacion.equals("+")){
+            System.out.println("Ingrese el número que quiere sumar");
+            this.num = input.nextDouble();
+            sumar();
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,17 @@ public class Main {
         String operacion;
 
         do {
+            // Menú para seleccionar la operación a realizar.
             System.out.println("Seleccione la operación que desea realizar:");
+            System.out.println(" + para sumar.");
+            System.out.println(" - para restar.");
+            System.out.println(" * para multiplicar.");
+            System.out.println(" / para dividir.");
+            System.out.println(" % para obtener el módulo");
+            System.out.println(" = para terminar");
             operacion = input.nextLine();
+            // llamado del método que define la operación segun lo ingresado por el usuario
+            System.out.println(calculadora.ejecutarOperacion(operacion));
         }while (!operacion.equals("="));
 
     }
